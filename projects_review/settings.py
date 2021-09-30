17a53d4accddadabc77b8a737e3b54854d3fbb80
k-koech <kelvin.koech@student.moringaschool.com>
2021-09-30 09:54:01 +0300

    Updated emails details

--- projects_review/settings.py
+++ projects_review/settings.py
@@ -29,15 +29,6 @@ EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-# EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-# EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
-# EMAIL_HOST = os.environ.get('EMAIL_HOST')
-# EMAIL_PORT = os.environ.get('EMAIL_PORT')
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
-
-
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
